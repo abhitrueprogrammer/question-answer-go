12195b5c31eb9a710961157e797146c1d384da0e
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-12-23 23:59:13 +0530

    improved up the code by adding types and parseInput function

--- quiz.go
+++ quiz.go
@@ -1,39 +1,56 @@
 package main
 
-import(
-	"os"
-	"fmt"
+import (
 	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-
-	fileName := "problems.csv"
-	file, err := os.Open(fileName)
+	fileName := flag.String("file", "problems.csv", "set custom filename")
+	flag.Parse()
+	file, err := os.Open(*fileName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "file read err: %v\n", err)
+		return
 	}
 	defer file.Close()
-	
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
+	problems := parseInput(records)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "csv err: %v\n", err)
+		return
     }
 	score := 0
 	input := ""
-	for _,line := range records {
-		fmt.Printf("%v: ", line[0])
+	for _,problem := range problems {
+
+		fmt.Printf("%v: ", problem.question)
 		fmt.Scanln(&input)
-		if(input == line[1]){
+		if(input == problem.answer){
 			fmt.Print("Correct input")
 			score +=1
 		}
 		input = ""
 	}
 	fmt.Printf("%v/%v", score, len(records))
-	// print("%v\n", reader.ReadAll())
-	// if len(os.Args) > 1{
+	
+}
 
-	// }
+func parseInput(records [][]string) []Problem {
+    var problems []Problem
+	for _,record := range records {
+		p := Problem {
+			question: record[0],
+			answer: record[1],
+		}
+		problems = append(problems, p)
+	}
+	return problems
+}
+type Problem struct{
+	question string
+	answer string
 }
