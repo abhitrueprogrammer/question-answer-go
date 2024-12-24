509b10e46a01faad4575dbdd1ac28ee5a4476463
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-12-25 01:58:23 +0530

    added a timer

--- quiz.go
+++ quiz.go
@@ -5,10 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	fileName := flag.String("file", "problems.csv", "set custom filename")
+	timeDuration := flag.Int("time", 10, "set custom timer")
+
 	flag.Parse()
 	file, err := os.Open(*fileName)
 	if err != nil {
@@ -22,35 +25,55 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "csv err: %v\n", err)
 		return
-    }
+	}
 	score := 0
 	input := ""
-	for _,problem := range problems {
+	duration := time.Duration(time.Duration(*timeDuration) * time.Second)
 
-		fmt.Printf("%v: ", problem.question)
-		fmt.Scanln(&input)
-		if(input == problem.answer){
-			fmt.Print("Correct input")
-			score +=1
+	Timer := time.NewTimer(duration)
+	done := make(chan struct{})
+
+	go func() {
+		<-Timer.C
+		done <- struct{}{}
+
+	}()
+
+	go func() {
+
+		for _, problem := range problems {
+
+			fmt.Printf("%v: ", problem.question)
+			fmt.Scanln(&input)
+			if input == problem.answer {
+				score += 1
+			}
+			input = ""
 		}
-		input = ""
-	}
+		done <- struct{}{}
+
+		Timer.Stop()
+
+	}()
+
+	<-done
 	fmt.Printf("%v/%v", score, len(records))
-	
+
 }
 
 func parseInput(records [][]string) []Problem {
-    var problems []Problem
-	for _,record := range records {
-		p := Problem {
+	var problems []Problem
+	for _, record := range records {
+		p := Problem{
 			question: record[0],
-			answer: record[1],
+			answer:   record[1],
 		}
 		problems = append(problems, p)
 	}
 	return problems
 }
-type Problem struct{
+
+type Problem struct {
 	question string
-	answer string
+	answer   string
 }
